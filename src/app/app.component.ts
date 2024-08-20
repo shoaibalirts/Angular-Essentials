73dfddfeffdb8aa044068eb3e7a07cf03572bbd9
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-20 12:55:49 +0200

    created Tasks component, event listened in user component and triggered in app and then sent data to Tasks component in order to use in its Tasks template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,21 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/HeaderComponent.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1'; // state initial value
+  get getSelectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
   onSelectUser1(id: string) {
-    console.log('selected user with id ' + id);
+    this.selectedUserId = id; // update state to new id when select event is triggered
   }
 }
