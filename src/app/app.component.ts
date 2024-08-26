7f362058729a6c17484ab13ac0583536a8989032
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-26 14:42:26 +0200

    created a new component and opened rendered it based on condition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import { HeaderComponent } from './header/HeaderComponent.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NewTaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
   selectedUserId?: string; // state initial value is undefined
+
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
   }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,24 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { dummyTasks } from './dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string; // accepted userId from app component
   @Input({ required: true }) name!: string;
+  @Output() addTask = new EventEmitter<string>();
   // @Input() name: string | undefined; // same as above
   tasks = dummyTasks;
   selectedTaskId?: string;
+
+  isAddingTask = false;
 
   get selectedUserTasks() {
     return this.tasks.filter((task) => this.userId === task.userId); // array of objects
   }
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+  onStartAddTask() {
+    console.log('clicked');
+    if (this.isAddingTask === false) {
+      this.isAddingTask = true;
+    } else {
+      this.isAddingTask = false;
+    }
   }
 }
