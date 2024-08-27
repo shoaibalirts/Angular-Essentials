0b42a4da5cdccedceb0341dca6ab42499fedb379
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-27 08:46:30 +0200

    used date pipe to tranform the date into another shape i.e. fullDate

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { type ParentTask } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
